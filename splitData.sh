dbc723d1119437a4501a47eea4fda553afa3813e
Yahan Chen <chen2254@wisc.edu>
2024-04-22 13:29:39 -0500

    change split 10 data to 50

--- splitData.sh
+++ splitData.sh
@@ -6,7 +6,7 @@ input_file="/workspace/linmaneechot/steam_data/english_reviews.csv"
 output_directory="/workspace/linmaneechot/steam_data/"
 
 Rscript -e "data = read.csv('$input_file'); \
-             data_split = split(data, cut(1:nrow(data), breaks = 10, label = FALSE)); \
+             data_split = split(data, cut(1:nrow(data), breaks = 50, label = FALSE)); \
              for (i in names(data_split)) { \
                  filename = paste0('$output_directory/output_', i, '.csv'); \
                  write.csv(data_split[[i]], filename, row.names = FALSE); \
