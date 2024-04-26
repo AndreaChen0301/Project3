851026ddaba9afe08d989508d721ac1eedbc1db7
Ben Schnoor <bjschnoor@wisc.edu>
2024-04-26 00:58:00 -0500

    pandas downloaded

---
+++ q3/submit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -f slurm_out/slurm*
+rm -f slurm*
+mkdir -p slurm_out/
+jobId1=$(sbatch --output="slurm_out/slurm-%A_%a.out" --error="slurm_out/slurm-%A_%a.err" bagofwords.sh)
+jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
+
+
+
