3b819b46f911cfad1473ee842a03b70370478660
BenSchnoor02 <90349470+BenSchnoor02@users.noreply.github.com>
2024-04-30 12:40:10 -0500

    Update submit.sh

--- q3/submit.sh
+++ q3/submit.sh
@@ -2,7 +2,7 @@
 rm -f slurm_out/slurm*
 rm -f slurm*
 mkdir -p slurm_out/
-jobId1=$(sbatch --output="slurm_out/slurm-%A_%a.out" --error="slurm_out/slurm-%A_%a.err" bagofwords.sh)
+jobId1=$(sbatch --array=1-50 --output="slurm_out/slurm-%A_%a.out" --error="slurm_out/slurm-%A_%a.err" bagofwords.sh)
 jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
 
 
