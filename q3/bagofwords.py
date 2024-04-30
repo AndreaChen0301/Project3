b6fed960c641ea0137e344a957a895755ea090ab
BenSchnoor02 <90349470+BenSchnoor02@users.noreply.github.com>
2024-04-30 12:32:50 -0500

    Update bagofwords.py

--- q3/bagofwords.py
+++ q3/bagofwords.py
@@ -1,3 +1,10 @@
-print('here')
-import pandas
-print('we have imported pandas')
+import pandas as pd
+import numpy as np
+#from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import linear_model
+from sklearn import metrics
+#import math
+input_dir="/workspace/linmaneechot/steam_data/"
