ad11d0f1288729eced5ade0e100d415fe0db3cfd
Pawin Linmaneechote <linmaneechot@wisc.edu>
2024-05-02 11:48:30 -0500

    new changes

--- q2/q2Analysis.sh
+++ q2/q2Analysis.sh
@@ -5,14 +5,14 @@ dest_dir="/workspace/linmaneechot/q2/allData"
 
 cat "$source_dir"/*.csv | tail -n +2  > "$dest_dir"/allData.csv
 
-Rscript -e "data = read.csv('$dest_dir/allData.csv', header = FALSE); \
-             data = na.omit(data)
+# Run R script and capture output
+Rscript -e "data = read.csv('$dest_dir/allData.csv'); \
+             data = na.omit(data); \
              data[,1] = as.numeric(data[,1]); \
              data[,2] = as.numeric(data[,2]); \
              life_min = data[,1]; \
              helpful_score = data[,2]; \
              cor_q2 = as.character(cor(life_min, helpful_score)); \
              sum_lm_model = as.character(summary(lm(life_min~helpful_score))); \
-             writeLines(cor_q2, paste0('$dest_dir','/cor_q2.txt')); \
-             writeLines(sum_lm_model, paste0('$dest_dir','/summary_lm.txt')) \
-"
+             cat(cor_q2, file = paste0('$dest_dir','/q2cor.txt')); \
+             cat(sum_lm_model, file = paste0('$dest_dir','/q2summary.txt'))"
